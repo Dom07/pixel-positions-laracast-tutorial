1c4727c44fc9ad984579a1144e49967f99896a94
Dominic Devasahayam <dominic5230@gmail.com>
2025-12-31 13:46:20 -0500

    More UI work

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,18 +1,18 @@
- <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+ <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group transition-colors duration-300">
     <div class="self-start text-sm">Company Name</div>
 
-    <div class="py-8 font-bold">
-        <h3>Title</h3>
-        <p>Full time - From $60,000</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-600 transition-colors duration-300 text-xl font-bold">Title</h3>
+        <p class="text-sm mt-4">Full time - From $60,000</p>
     </div>
 
-    <div class="flex justify-between item-center mt-auto">
+    <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag>Tag</x-tag>   
-            <x-tag>Tag</x-tag>   
-            <x-tag>Tag</x-tag>   
+            <x-tag size="small">Tag</x-tag>
+            <x-tag size="small">Tag</x-tag>
+            <x-tag size="small">Tag</x-tag>
         </div>
 
-        <img src="https://placehold.co/42x42" alt="">
+        <img src="https://placehold.co/42x42" alt="" class="rounded-xl">
     </div>
 </div>
